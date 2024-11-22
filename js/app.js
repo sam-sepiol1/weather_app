fb5702da9191874fedd0db20459dd9fa27a7de10
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-11-23 00:01:56 +0100

    Basic features done

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let API_CITIES_URL;
 const searchInput = document.querySelector('.search--form_input');
 const searchButton = document.querySelector('.search--form_button');
 const weatherInfo = document.querySelector('.weather');
+const flags = await getFlags();
 
 let city_name = "";
 
@@ -17,31 +18,79 @@ async function getCities(city_name) {
     
 }
 
+async function getWeather(lat, lon) {
+    API_WEATHER_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const response = await fetch(API_WEATHER_URL);
+    const weather = await response.json();
+    return weather;
+}
+
+async function getFlags() {
+    return fetch('../js/flags.json')
+        .then(response => response.json())
+        .catch(error => console.error(error));
+}
+
+// function emoji(weather) {
+//     switch (weather.) {
+//         case value:
+            
+//             break;
+    
+//         default:
+//             break;
+//     }
+// }
+
 searchInput.addEventListener('keyup', async (e) => {
+
+    
     city_name = e.target.value;    
     let cities = await getCities(city_name);
-    cities.forEach(city => {
+
+    for (const city of cities) {
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            continue;
+        }
         const option = document.createElement('option');
-        option.value = city.name;
+        option.value = city.name + ', ' + city.country + ', ' + city.state;
         document.querySelector('#cities').appendChild(option);
-    })
+    }
 });
 
 searchButton.addEventListener('click', async (e) => {
     e.preventDefault();
-    API_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
-    const response = await fetch(API_WEATHER_URL);
-    const data = await response.json();
-    console.log(data);
-    const temp = Math.round(data.main.temp - 273.15);
-    const weather = `
-    <div class="weather--card">
-        <h2 class="weather--card_title">${data.name}</h2>
-        <p class="weather--card_temp">${temp}°C</p>
-        <p class="weather--card_description">${data.weather[0].description}</p>
-    </div>
-    `;
-    weatherInfo.innerHTML = weather;
+    city_name = searchInput.value;
+    let city = await getCities(city_name);
+    let lat = city[0].lat;
+    let lon = city[0].lon;
+    let weather = await getWeather(lat, lon);
+    let temperature = Math.round(weather.list[0].main.temp - 273.15);
+
+
+    let card = document.createElement('div');
+    card.classList.add('weather--card');
+
+    let title = document.createElement('h2');
+    title.classList.add('weather--card_title');
+    title.textContent = city_name;
+
+    let temp = document.createElement('p');
+    temp.classList.add('weather--card_temp');
+    temp.textContent = temperature + '°C';
+
+    let description = document.createElement('p');
+    description.classList.add('weather--card_description');
+    description.textContent = weather.list[0].weather[0].description;
+
+    card.appendChild(title);
+    card.appendChild(temp);
+    card.appendChild(description);
+
+    weatherInfo.appendChild(card);
+    console.log(weather);
+    
+
 });
 
 

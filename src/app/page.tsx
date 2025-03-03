814458dd2b239a3c02767b5afba0600845456014
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-03-03 17:32:27 +0100

    FEAT : implement location and weather fetching with state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,58 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Clock from '@/components/Clock';
 import Search_Form from '@/components/Search_form';
+import Card from '@/components/Card';
 
 import { getLocation } from '@/utils/getLocation';
+import { getWeather, getLocationWeather } from '@/utils/getWeather';
 
 export default function Home() {
+    const [location, setLocation] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
+
+    async function fetchLocation() {
+        const location = await getLocation();
+       
+        
+        if (!location) {
+            console.log('No location found');
+            return;
+        }        
+        setLocation(location);
+    }
+
+    async function fetchWeather() {
+        if (location) {
+            const weather = await getLocationWeather(location);
+            if (!weather) {
+                console.log('No weather found');
+                return;
+            }
+            setWeatherData(weather);
+        }
+    }
+
+
+
     useEffect(() => {
-        getLocation();
+        fetchLocation();
     }, []);
+    
+  
+    useEffect(() => {
+        fetchWeather();
+    }, [location]);
 
     return (
-        <main className="h-screen flex flex-col justify-center">
+        <main className="h-screen flex flex-col gap-4 justify-center items-center">
             <div className="flex flex-col gap-4 justify-center items-center">
                 <Clock />
                 <Search_Form />
             </div>
+                {weatherData && <Card weatherData={weatherData} />}
         </main>
     );
 }

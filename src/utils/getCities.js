7b78db205cf7c0f635007dcf807da3d59fb38ad2
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-03-04 16:10:23 +0100

    FIX : declare API_CITIES_URL with let for proper scoping in getCities function

--- src/utils/getCities.js
+++ src/utils/getCities.js
@@ -1,5 +1,5 @@
 async function getCities(city_name) {
-    API_CITIES_URL = `https://13-weather-api.vercel.app/cities/${city_name}`;
+    let API_CITIES_URL = `https://13-weather-api.vercel.app/cities/${city_name}`;
     try {
         const response = await fetch(API_CITIES_URL);
         const data = await response.json();
